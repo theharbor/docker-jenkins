1f9106988b48e9c608ab4c937f41c57ab8960db2
Nick Groenen <nick@groenen.me>
2015-04-05 22:51:34 +0200

    Install curl

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,8 @@ rm /jenkins-ci.org.key
 
 echo 'deb http://pkg.jenkins-ci.org/debian binary/' > /etc/apt/sources.list.d/jenkins-ci.list
 apt-get update
-apt-get install --no-install-recommends -y openssh-client ca-certificates jenkins git bzr subversion mercurial
+apt-get install --no-install-recommends -y openssh-client ca-certificates jenkins
+apt-get install --no-install-recommends -y git bzr subversion mercurial curl
 rm -rf /var/lib/apt/lists/*
 
 echo '    StrictHostKeyChecking no' >> /etc/ssh/ssh_config
