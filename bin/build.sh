44e48ddd523cffed3bb177f2d66cf74e7606c174
Nick Groenen <nick@groenen.me>
2015-04-05 22:43:00 +0200

    Install openssh-client

--- bin/build.sh
+++ bin/build.sh
@@ -2,14 +2,13 @@
 
 set -e
 set -x
-alias aptinstall="apt-get install --no-install-recommends --yes"
 
 apt-key add - < /jenkins-ci.org.key
 rm /jenkins-ci.org.key
 
 echo 'deb http://pkg.jenkins-ci.org/debian binary/' > /etc/apt/sources.list.d/jenkins-ci.list
 apt-get update
-aptinstall ca-certificates jenkins git bzr subversion mercurial
+apt-get install --no-install-recommends -y openssh-client ca-certificates jenkins git bzr subversion mercurial
 rm -rf /var/lib/apt/lists/*
 
 echo '    StrictHostKeyChecking no' >> /etc/ssh/ssh_config
